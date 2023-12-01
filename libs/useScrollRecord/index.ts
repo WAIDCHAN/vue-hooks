39dc98a8080693103f6158837b623a8ed62b32b1
WEID <chen@weid.ink>
2023-12-01 10:56:18 +0800

    init: init the original project

---
+++ libs/useScrollRecord/index.ts
@@ -0,0 +1,41 @@
+import { ref } from "vue";
+import { throttle } from "lodash";
+
+export type ScrollPosition = {
+    x: number;
+    y: number;
+}
+
+export function useScrollRecord() {
+    const position = ref<ScrollPosition>({x: 0, y: 0})
+    const scrollContent = ref<HTMLElement | null>(null);
+
+    function registerScroll():()=> void {
+        if(scrollContent.value) {
+            const el: HTMLElement = scrollContent.value;
+            const handleScroll = throttle(():void=> {
+                position.value.x = el.scrollLeft;
+                position.value.y = el.scrollTop;
+            }, 100, { trailing: true });
+            scrollContent.value.addEventListener(handleScroll);
+            return ():void => removeEventListener(handleScroll);
+        }
+        return ():void=> {};
+    }
+
+    function rollbackScroll(recordPosition: ScrollPosition) {
+        if(!scrollContent.value) return;
+        if(Object.prototype.hasOwnProperty.call(recordPosition, 'x')) {
+            scrollContent.value.scrollLeft = recordPosition.x;
+        }
+        if(Object.prototype.hasOwnProperty.call(recordPosition, 'y')) {
+            scrollContent.value.scrollTop = recordPosition.y;
+        }
+    }
+
+    return {
+        registerScroll,
+        rollbackScroll,
+        scrollContent
+    }
+}
